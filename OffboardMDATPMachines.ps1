79fb60bea590d2cbbd6c38dd4a6d5559e99bf85d
scotcham <50599279+scotcham@users.noreply.github.com>
2020-08-19 16:14:50 -0500

    Add files via upload
    This tiny script will offboard Windows 10 machines (in a machines list) from the MDATP portal.  Note - if the Windows Defender Advanced Threat Protection service is restarted on the machine(s), the machine will re-onboard.  This is by design. 
    
    You can set the sense service to disabled in the registry to prevent the service from restarting.

---
+++ OffboardMDATPMachines.ps1
@@ -0,0 +1,30 @@
+$tenantId = '<tenant ID>' ### Paste your own tenant ID here
+$appId = '<app ID>' ### Paste your own app ID here
+$appSecret = '<app Secret>' ### Paste your own app keys here
+$resourceAppIdUri = 'https://api.securitycenter.windows.com'
+$oAuthUri = "https://login.windows.net/$TenantId/oauth2/token"
+
+$authBody = [Ordered] @{
+    resource = "$resourceAppIdUri"
+    client_id = "$appId"
+    client_secret = "$appSecret"
+    grant_type = 'client_credentials'
+}
+
+$gett=Invoke-WebRequest -Uri https://login.windows.net/$tenantId/oauth2/token -Method POST -Body $authBody
+$gett=($gett.Content | ConvertFrom-Json)
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$contentType = "application/json"
+$token=$gett.access_token
+$header = @{"Authorization"="Bearer $token"}
+
+$machineList = Get-Content C:\temp\MachineList.txt #######  Paste in the location of a list of machines
+
+$json = @{'Comment' = 'Offboard'} | ConvertTo-Json
+
+foreach($machine in $machineList)
+{
+    $machine    
+    $answer = Invoke-RestMethod -Headers $header -Uri  https://api-us.securitycenter.windows.com/api/machines/$machine/offboard -Method POST -Body $json -ContentType $contentType
+}
