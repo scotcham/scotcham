1d070a8d944e0cbd8710a372bf12fb1241ec5748
scotcham <50599279+scotcham@users.noreply.github.com>
2020-10-09 12:26:44 -0500

    Add files via upload
    This script unisolates machines using a list of machine IDs.

---
+++ UnIsolateMDATPMachines.ps1
@@ -0,0 +1,34 @@
+# Author - ScottC
+# RUN AT YOUR OWN RISK
+# This script will unisolate machines in MDATP using
+# the MDATP API
+$tenantId = '<tenant ID>' ### Paste your own tenant ID here
+$appId = '<app ID>' ### Paste your own app ID here
+$appSecret = '<app Secret>' ### Paste your own app keys here
+$resourceAppIdUri = 'https://api.securitycenter.windows.com'
+$oAuthUri = "https://login.windows.net/$TenantId/oauth2/token"
+
+$authBody = [Ordered] @{
+    resource = "$resourceAppIdUri"
+    client_id = "$appId"
+    client_secret = "$appSecret"
+    grant_type = 'client_credentials'
+}
+
+$gett=Invoke-WebRequest -Uri https://login.windows.net/$tenantId/oauth2/token -Method POST -Body $authBody
+$gett=($gett.Content | ConvertFrom-Json)
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$contentType = "application/json"
+$token=$gett.access_token
+$header = @{"Authorization"="Bearer $token"}
+
+$machineList = Get-Content C:\temp\MachineList.txt #######  Paste in the location of a list of machines
+
+$json = @{'Comment' = 'Un Isolate Machines using a list of machines'} | ConvertTo-Json
+
+foreach($machine in $machineList)
+{
+    $machine    
+    $answer = Invoke-RestMethod -Headers $header -Uri  https://api-us.securitycenter.windows.com/api/machines/$machine/unisolate -Method POST -Body $json -ContentType $contentType
+}
